fd0bcac8ecd3e99063e95860491c53aaaf8a2882
Dawid Dziurla <dawidd0811@gmail.com>
2021-06-16 06:58:14 +0200

    Update etcd.rb

--- Formula/etcd.rb
+++ Formula/etcd.rb
@@ -17,6 +17,7 @@ class Etcd < Formula
   depends_on "go" => :build
 
   def install
+    system "go", "mod", "vendor"
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"),
            "-o", bin/"etcd"
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"),
