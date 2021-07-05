04404982aaa935bc0425e9cc08b651a991210bcc
Dawid Dziurla <dawidd0811@gmail.com>
2021-07-05 18:47:42 +0200

    workflows: split tests jobs

--- Formula/zlib.rb
+++ Formula/zlib.rb
@@ -6,6 +6,7 @@ class Zlib < Formula
   sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
   license "Zlib"
   head "https://github.com/madler/zlib.git", branch: "develop"
+  revision 1
 
   livecheck do
     url :homepage
